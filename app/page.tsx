5ac27634e033ab0a2ddb11167dbad693fdb8a29f
Mayur Chougule <mayurchougule1@gmail.com>
2024-03-08 11:42:05 -0500

    domain verification address

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,8 @@ export default function Home() {
               </a>
               <a className="chakra-button css-9syocv" href="https://docs.vistara.dev" target="_blank">Docs<span className="chakra-button__icon css-1hzyiq5"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" aria-hidden="true" focusable="false" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h6zm11-3v9l-3.794-3.793-5.999 6-1.414-1.414 5.999-6L12 3h9z"></path></g></svg></span>
               </a>
-            </div>
+            </div><br />
+            <p>Vistara Labs, Inc. <br /> 4 Trestle Way, Dover, NH 03820 <br />Email: mayur@vistara.dev <br />Website: https://www.vistara.dev</p>
             {/* Why Vistara? */}
 
         </main>
